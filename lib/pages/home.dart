3a12dfe407defc5242b1c80c0bf97f37bbd1c22c
adolesamuel <adolesamuel4@gmail.com>
2021-02-12 12:15:52 +0100

    added page slide animation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -73,7 +73,8 @@ class _HomeState extends State<Home> {
   }
 
   onTap(int pageIndex) {
-    pageController.jumpToPage(pageIndex);
+    pageController.animateToPage(pageIndex,
+        duration: Duration(milliseconds: 200), curve: Curves.easeInOut);
   }
 
   @override
