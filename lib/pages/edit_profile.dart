67f9b8ef3495b10f4235b0b29a33e966b687b217
adolesamuel <adolesamuel4@gmail.com>
2021-02-18 12:41:25 +0100

    profile page designed

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -1,13 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:flutter/material.dart";
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/home.dart';
 
 class EditProfile extends StatefulWidget {
+  final String currentUserId;
+
+  EditProfile({this.currentUserId});
   @override
   _EditProfileState createState() => _EditProfileState();
 }
 
 class _EditProfileState extends State<EditProfile> {
+  bool isLoading = false;
+  User user;
+
+  TextEditingController displayNameController = TextEditingController();
+  TextEditingController bioController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    getUser();
+  }
+
+  getUser() async {
+    setState(() {
+      isLoading = true;
+    });
+    DocumentSnapshot doc = await usersRef.document(widget.currentUserId).get();
+    user = User.fromDocument(doc);
+    displayNameController.text = user.displayName;
+    bioController.text = user.bio;
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Edit Profile');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Edit Profile",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
   }
 }
