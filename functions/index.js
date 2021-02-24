fba8d01f4b00109183d9797b7ef305590602421f
adolesamuel <adolesamuel4@gmail.com>
2021-02-24 12:26:41 +0100

    awaiting google firebase account upgrade

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +9,36 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+exports.onCreateFollower = functions.firestore
+    .document("/followers/{userId}/userFollowers/{followeId}").onCreate(async (snapshot, context) => {
+        console.log("follower created", snapshot.data());
+        const userId = context.params.userId;
+        const followerId = context.params.followerId;
+
+        //1 create followed users post reference
+        const followedUserPostsRef = admin
+            .firestore()
+            .collection('posts')
+            .doc(userId)
+            .collection('userPosts');
+
+        //2  Create follwoing users timeline ref
+        const timelinePostsRef = admin
+            .firestore()
+            .collection('timeline')
+            .doc(followerId)
+            .collection('timelinePosts');
+
+        //3 Get the followed users posts
+        const querySnapshot = await followedUserPostsRef.get();
+
+        //4 Add each user post to following user's timeline
+        querySnapshot.forEach(doc => {
+            if (doc.exists) {
+                const postId = doc.id;
+                const postData = doc.data();
+                timelinePostsRef.doc(postId).set(postData);
+            }
+        })
+
+    });
