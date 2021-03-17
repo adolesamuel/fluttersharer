f4a3737543612089a7a197a9155c1e6d9169fcce
adolesamuel <adolesamuel4@gmail.com>
2021-03-17 20:13:51 +0100

    timeline in progress

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,6 +19,7 @@ final activityFeedRef = Firestore.instance.collection('feed');
 final commentsRef = Firestore.instance.collection('comments');
 final followersRef = Firestore.instance.collection('followers');
 final followingRef = Firestore.instance.collection('following');
+final timelineRef = Firestore.instance.collection('timeline');
 final DateTime timestamp = DateTime.now();
 User currentUser;
 
@@ -133,11 +134,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: PageView(
         children: <Widget>[
-          // Timeline(),
-          RaisedButton(
-            onPressed: logout,
-            child: Text('Logout'),
-          ),
+          Timeline(currentUser: currentUser),
           ActivityFeed(),
           Upload(currentUser: currentUser),
           Search(),
